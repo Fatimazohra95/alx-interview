66ba46e45b3bcbd31ee3fb41c20f25d9eabe0eea
Fatimazohra95 <fatimazohralakhal32@gmail.com>
2024-02-08 17:40:04 +0100

    Python

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,25 @@
+#!/usr/bin/python3
+"""Pascal"""
+
+
 def pascal_triangle(n):
-    """Function to generate Pascal's triangle up to depth n"""
+    """fucnrion to get the number of pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = []
+    pascal_triangle = [0] * n
 
     for i in range(n):
-        new_row = [0] * (i + 1)
+        new_row = [0] * (i+1)
         new_row[0] = 1
-        new_row[-1] = 1
+        new_row[len(new_row) - 1] = 1
 
-        for j in range(1, len(new_row) - 1):
-            a = triangle[i - 1][j]
-            b = triangle[i - 1][j - 1]
-            new_row[j] = a + b
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-        triangle.append(new_row)
+        pascal_triangle[i] = new_row
 
-    return triangle
+    return pascal_triangle
